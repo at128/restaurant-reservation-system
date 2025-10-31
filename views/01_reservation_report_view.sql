1f9ca1214b48b8cea903a01acd7bdf0eee918f86
Atta Alarayshi <mr.attamazen@gmail.com>
2025-10-31 13:53:43 +0200

    Add database views for reporting
    - vw_ReservationReport: Comprehensive reservation details with customer and restaurant info
    - vw_EmployeesWithRestaurants: Employee assignments with restaurant details
    - Both views include proper DROP IF EXISTS statements
    - Added column aliases for readability

---
+++ views/01_reservation_report_view.sql
@@ -0,0 +1,24 @@
+-- ========================================
+-- View: vw_ReservationReport
+-- ========================================
+-- Description: Comprehensive view of all reservations
+--              with customer, restaurant, and table details
+-- ========================================
+
+DROP VIEW IF EXISTS vw_ReservationReport;
+GO
+
+CREATE VIEW vw_ReservationReport AS
+SELECT 
+    r.ReservationID,
+    r.ReservationDate AS [Reservation Date],
+    rest.Name AS [Restaurant Name],
+    rest.City AS [Restaurant City],
+    t.Capacity AS [Table Capacity],
+    r.PartySize AS [Number of Guests],
+    cu.FirstName + ' ' + cu.LastName AS [Customer Name]
+FROM Reservations r
+INNER JOIN Tables t ON t.TableId = r.TableID
+INNER JOIN Restaurants rest ON t.RestaurantId = rest.RestaurantId
+INNER JOIN Customers cu ON cu.CustomerID = r.CustomerID;
+GO
---
+++ views/02_employees_with_restaurants_view.sql
@@ -0,0 +1,19 @@
+-- ========================================
+-- View: vw_EmployeesWithRestaurants
+-- ========================================
+-- Description: Lists all employees with their restaurant assignments
+-- ========================================
+
+DROP VIEW IF EXISTS vw_EmployeesWithRestaurants;
+GO
+
+CREATE VIEW vw_EmployeesWithRestaurants AS
+SELECT 
+    e.FirstName + ' ' + e.LastName AS [Employee Name],
+    e.Position,
+    r.Name AS [Restaurant Name],
+    r.OpeningHours AS [Restaurant Opening Hours]
+FROM Employees e
+INNER JOIN Restaurants r 
+    ON r.RestaurantID = e.RestaurantID;
+GO
